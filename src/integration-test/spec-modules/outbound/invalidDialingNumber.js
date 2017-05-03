a80f3b2ed2df3abc9311fcacbbb45ea33756c627
marcusrc <marcus.chen@ringcentral.com>
2017-05-03 23:17:31 +0800

    fix codacy

--- src/integration-test/spec-modules/outbound/invalidDialingNumber.js
+++ src/integration-test/spec-modules/outbound/invalidDialingNumber.js
@@ -2,7 +2,9 @@ import { ensureLogin, containsErrorMessage } from '../../utils/HelpUtil';
 import callErrors from '../../../modules/Call/callErrors';
 
 export default (Auth, Alert, Client, RegionSettings, Call, testAccount, caseId = '') => {
-  const testAccounts = testAccount && testAccount.constructor === Array ? testAccount : [testAccount];
+  const testAccounts = testAccount && testAccount.constructor === Array ?
+    testAccount :
+    [testAccount];
   testAccounts.forEach((account) => {
     describe(`${caseId} Outbound call - Error handling for invalid dialing number', ${account.accountType ? `(${account.accountType})` : ''}`, function () {
       this.timeout(10000);
@@ -13,8 +15,8 @@ export default (Auth, Alert, Client, RegionSettings, Call, testAccount, caseId =
           console.error('Skip test case as failed to login with credential ', account);
         }
       });
-      it('Should Alert Invalid Number - Invalid Char in ToNumber', async() => {
-        Call.onToNumberChange("iamn%@onedi!@$%^&()_=\\][';/.,~nu><.,,?/mber#*");
+      it('Should Alert Invalid Number - Do not enter anything', async () => {
+        Call.onToNumberChange('');
         await Call.onCall();
         expect(containsErrorMessage(Alert.state.messages, callErrors.noToNumber))
           .to.not.equal(undefined);
@@ -25,8 +27,8 @@ export default (Auth, Alert, Client, RegionSettings, Call, testAccount, caseId =
         expect(containsErrorMessage(Alert.state.messages, callErrors.notAnExtension))
           .to.equal(undefined);
       });
-      it('Should Alert Invalid Number - Do not enter anything', async() => {
-        Call.onToNumberChange('');
+      it('Should Alert Invalid Number - Invalid Char in ToNumber', async () => {
+        Call.onToNumberChange("iamn%@onedi!@$%^&()_=\\][';/.,~nu><.,,?/mber#*");
         await Call.onCall();
         expect(containsErrorMessage(Alert.state.messages, callErrors.noToNumber))
           .to.not.equal(undefined);
