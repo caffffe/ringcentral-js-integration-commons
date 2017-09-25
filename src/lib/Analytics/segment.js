61c963d1a44beae6161194bca034f04b619f3bb1
caffffe <sophie.wu@rcoffice.ringcentral.com>
2017-09-25 09:52:20 +0800

    codacy-bot

--- src/lib/Analytics/segment.js
+++ src/lib/Analytics/segment.js
@@ -54,7 +54,6 @@ export default function init() {
           const script = document.createElement('script');
           script.type = 'text/javascript';
           script.async = true;
-          // script.src = `${document.location.protocol}//cdn.segment.com/analytics.js/v1/${key}/analytics.min.js`;
           script.src = `https://cdn.segment.com/analytics.js/v1/${key}/analytics.min.js`;
           
           // Insert our script next to the first script element.
